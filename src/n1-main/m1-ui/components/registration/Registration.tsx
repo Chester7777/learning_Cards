c8e020a8e7bde4901fd77ad67ac09c6312a39613
dg1886 <dgarbuz1888@gmail.com>
2021-05-31 20:48:16 +0300

    Layout Registration.tsx; Сreating styles in Registration.module.css;

--- src/n1-main/m1-ui/components/registration/Registration.tsx
+++ src/n1-main/m1-ui/components/registration/Registration.tsx
@@ -5,25 +5,38 @@ import {Button} from "../../common/Button/Button";
 
 const Registration = () => {
 
-///fdfdfdfdfdfdfdfdfdfdf
-        return (
-            <div>
-                <p> Please fill in the blank fields and press sign up </p>
-                <form className={s.register}>
 
-                    <div>Email</div>
-                    <input />
-                    <div >Password</div>
-                    <input />
-                    <div >Password</div>
-                    <input />
+    return (
+        <div className={s.background}>
+            <div className={s.registerBlock}>
+                <p> Please fill in the blank fields and press sign up </p>
+
+                <form className={s.registerForm}>
+
+                    <label htmlFor={'email'}>Email</label>
+                    <div className={s.register}>
+                    <input className={s.registerFormInput}  type="email"
+
+                    />
+                    </div>
+
+                    <label htmlFor={'password'}>Password</label>
+                    <div className={s.register}>
+                    <input className={s.registerFormInput} type="password"/>
+                    </div>
+
+                    <label htmlFor={'password'}>Confirm password</label>
+                    <div className={s.register}>
+                    <input className={s.registerFormInput} type="password"/>
+                    </div>
                     <Button
                         label={'Sign Up'}
                         backgroundColor={'blue'}/>
                 </form>
             </div>
+        </div>
 
-        )
+    )
 
 }
 
