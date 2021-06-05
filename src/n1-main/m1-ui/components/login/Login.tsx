63abf97154fa0c7f0966f5ef9e0f16b8ae4ae26c
Alexey Khazan <khazanalexey@hotmail.com>
2021-06-05 23:56:05 +0300

    validation

--- src/n1-main/m1-ui/components/login/Login.tsx
+++ src/n1-main/m1-ui/components/login/Login.tsx
@@ -116,6 +116,7 @@ const LoginContent: React.FC<LoginContentPropsType> = ({
                         onClick={onClickLoginButton}
                         label={'Login'}
                     />
+
                     <div className={s.errorText}>{error}</div>
 
                 </form>
